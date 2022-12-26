b294debf99d186153a52fc874f1f8a6280d3d263
Pedro Machado de Paula <pmdpaula@gmail.com>
2022-12-26 19:37:28 -0300

    finalizando desafio 02 - swagger

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -15,7 +15,7 @@ class ShowUserProfileUseCase {
       user = this.usersRepository.findByEmail(user_id);
 
       if (!user) {
-        throw new Error("Unauthorized");
+        throw new Error("User not found!");
       }
     }
 
